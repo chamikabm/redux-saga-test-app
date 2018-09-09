e898a3eca7f845af9604701ae5f9455e341dcfd9
chamikab <kasun.chamika@gmail.com>
2018-09-09 14:05:57 +0530

    First auth saga related changes added.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+
+import { watchAuth } from "./store/sagas/index";
 
 import './index.css';
 import App from './App';
@@ -20,9 +23,13 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
 const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, sagaMiddleware)
 ));
+
+sagaMiddleware.run(watchAuth);
 
 const app = (
     <Provider store={store}>
