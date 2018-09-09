e45124aac23be80d84a1baff00ff5468227089d0
chamikab <kasun.chamika@gmail.com>
2018-09-09 22:22:11 +0530

    Added changes for advance redux saga.

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,15 +1,22 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, all, takeLatest } from 'redux-saga/effects';
 
 import * as actionTypes from '../actions/actionTypes';
-import { logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga } from "./auth";
-import { initIngredientsSaga } from './burgerBuilder';
-import { purchaseBurgerSaga, fetchOrdersSaga } from './order';
+import {logoutSaga, checkAuthTimeoutSaga, authUserSaga, authCheckStateSaga} from "./auth";
+import {initIngredientsSaga} from './burgerBuilder';
+import {purchaseBurgerSaga, fetchOrdersSaga} from './order';
 
 export function* watchAuth() {
-  yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
-  yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
-  yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
-  yield takeEvery(actionTypes.AUTH_CHECK_INITIAL_STATE, authCheckStateSaga);
+  // yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+  // yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
+  // yield takeEvery(actionTypes.AUTH_USER, authUserSaga);
+  // yield takeEvery(actionTypes.AUTH_CHECK_INITIAL_STATE, authCheckStateSaga);
+  // Above yields can be achieved also as follows.
+  yield all([
+    takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga),
+    takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga),
+    takeEvery(actionTypes.AUTH_USER, authUserSaga),
+    takeEvery(actionTypes.AUTH_CHECK_INITIAL_STATE, authCheckStateSaga)
+  ]);
 }
 
 export function* watchBurgerBuilder() {
@@ -17,7 +24,8 @@ export function* watchBurgerBuilder() {
 }
 
 export function* watchOrder() {
-  yield takeEvery(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
+  // We can use takeLatest instead takeEvery, but in takeLatest it will cancel all current actions and continue with the latest.
+  yield takeLatest(actionTypes.PURCHASE_BURGER, purchaseBurgerSaga);
   yield takeEvery(actionTypes.FETCH_ORDERS, fetchOrdersSaga);
 }
 
