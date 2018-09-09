155b35630f81529470d209eb6d0f80919e660993
chamikab <kasun.chamika@gmail.com>
2018-09-09 13:36:50 +0530

    Project configured to work with firebase.

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,10 +48,12 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
-        if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyB5cHT6x62tTe-g27vBDIqWcwQWBSj3uiY';
-        }
+
+      let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCyhj6r00ro3Z4eACeKftM2fj8fy5bACpE';
+
+      if (!isSignup) {
+        url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCyhj6r00ro3Z4eACeKftM2fj8fy5bACpE';
+      }
         axios.post(url, authData)
             .then(response => {
                 const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
